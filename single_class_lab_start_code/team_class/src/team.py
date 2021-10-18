50397d6517c2c70b206acd9c8fa5eaaa19978782
iain <iainfraser188@msn.com>
2021-10-18 14:15:40 +0100

    first two team tests

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,18 @@
+class Team():
+
+    def __init__(self,name,players,coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+
+    def team_has_name(name):
+        return name
+    
+    def team_has_players(players):
+        return players
+
+    def team_has_coach(coach):
+        return coach    
+
+    def coach_can_be_changed(coach):
+        return coach    
