d2e98168ef4dfa98aa8eec7b81d04d0d7bbcd0f4
iain <iainfraser188@msn.com>
2021-10-18 14:47:22 +0100

    all tests done

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -1,9 +1,13 @@
 class Team():
+    
+    # points = 0
 
     def __init__(self,name,players,coach):
         self.name = name
         self.players = players
         self.coach = coach
+        self.points = 0
+        
 
     def team_has_name(name):
         return name
@@ -16,3 +20,27 @@ class Team():
 
     def coach_can_be_changed(coach):
         return coach    
+    
+    def add_player(self,new_player):
+        self.players.append(new_player)
+        return self.players
+
+    def has_player(self,player):
+        for p in self.players:
+
+         if p == player:
+            
+            return True
+        else: 
+            return False    
+
+    def team_has_points(points):
+        return points
+    
+    def play_game(self,result):
+        if result == True:
+            self.points += 3
+            
+        
+             
+            
