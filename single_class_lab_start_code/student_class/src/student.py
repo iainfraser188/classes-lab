13d050621ec6d628bbf836007dd6ac3b9bbebae1
iain <iainfraser188@msn.com>
2021-10-18 13:57:41 +0100

    first tests done

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,6 +1,22 @@
-class Student(Name,Cohort):
+class Student:
     def __init__(self,Name,Cohort):
-        self.Name = Name
-        self.Cohort = Cohort
+        self.name = Name
+        self.cohort = Cohort
 
+    def talk(self):
+        return "I can talk!"
+
+    def student_has_name(self,Name):
+        return Name
+
+    def student_has_cohort(self,Cohort):
+        return Cohort
+    
+    def student_can_update_name(self,Name):
+        return Name
+
+    def student_can_change_cohort(self,Cohort):
+        return Cohort       
         
+
+
